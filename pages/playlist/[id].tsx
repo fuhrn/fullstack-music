92a86826eeacdca9d8a98d915c3c8e305d8bc963
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-06-08 06:29:47 -0300

    Auth error handling

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -36,12 +36,23 @@ const Playlist = ({ playlist }) => {
 };
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  let user
+  try {
+     user = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  } catch (error) {
+    return {
+      redirect: {
+        permanent: false,
+        path: "/signin",
+      },
+    };
+  }
+ 
   const [playlist] = await prisma.playlist.findMany({
     where: {
       // para convertir id de string a number
       id: +query.id,
-      userId: id,
+      userId: user.id,
     },
     include: {
       songs: {
